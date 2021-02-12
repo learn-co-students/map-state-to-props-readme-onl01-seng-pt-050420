5db2cb4ab8d179c16424682e7da3a4e9f7595d08
ParishBen <ParishBen13@gmail.com>
2021-02-11 19:17:06 -0500

    Done.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'; 
 import './App.css';
 
 class App extends Component {
@@ -18,7 +19,12 @@ class App extends Component {
         <p>{this.props.items.length}</p>
       </div>
     );
-  }
+  } 
 };
 
-export default App;
+const mapStateToProps = (state) => {
+  return {items: state.items};
+}
+
+
+export default connect(mapStateToProps)(App);
